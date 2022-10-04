e5fa28b9375270162ef8a66a45f8ef5ddadfc364
Isigba Ambrose <ambrisexyz@gmail.com>
2022-10-04 19:31:03 +0200

    added decorator to codebase

--- name.rb
+++ name.rb
@@ -1,27 +1,31 @@
-class Nameable 
-    def correct_name
-        raise NotImplementedError
-    end
+class Nameable
+  def correct_name
+    raise NotImplementedError
+  end
 end
 
 class BaseDecorator < Nameable
-attr_accessor :nameable
-def initialize(nameable)
+  attr_accessor :nameable
+
+  def initialize(nameable)
     super()
     @nameable = nameable
-end
-def correct_name
+  end
+
+  def correct_name
     @nameable.correct_name
+  end
 end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
-class  CapitalizeDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name.capitalize
-    end
-end
-class  TrimmerDecorator < BaseDecorator
-    def correct_name
-        @nameable.correct_name[0...10] if @nameable.correct_name.length > 10
-        return @nameable.correct_name 
-    end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name[0...10] if @nameable.correct_name.length > 10
+    @nameable.correct_name
+  end
 end
--- person.rb
+++ person.rb
@@ -1,36 +1,42 @@
-class Nameable 
+class Nameable
   def correct_name
-      raise NotImplementedError
+    raise NotImplementedError
   end
 end
 
-
 class BaseDecorator < Nameable
   attr_accessor :nameable
+
   def initialize(nameable)
-      super()
-      @nameable = nameable
+    super()
+    @nameable = nameable
   end
+
   def correct_name
-      @nameable.correct_name
+    @nameable.correct_name
   end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
   end
-  class  CapitalizeDecorator < BaseDecorator
-      def correct_name
-          @nameable.correct_name.capitalize
-      end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    return @nameable.correct_name[0...10] if @nameable.correct_name.length > 10
+
+    @nameable.correct_name
   end
-  class  TrimmerDecorator < BaseDecorator
-      def correct_name
-        return  @nameable.correct_name[0...10] if @nameable.correct_name.length > 10
-          return @nameable.correct_name 
-      end
-  end
+end
+
 class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -40,6 +46,7 @@ class Person < Nameable
   def correct_name
     @name
   end
+
   def of_age?
     if @age >= 18
       true
@@ -55,18 +62,10 @@ class Person < Nameable
   end
   public :can_use_services?
 end
-=begin
-person = Person.new('Ambrose', 37)
-puts person.name
 
-person.name = 'JOHN'
-person.age = 33
-puts person.name
-puts person.age
-=end
 person = Person.new(22, 'maximilianus')
-  puts person.correct_name
- capitalizedPerson = CapitalizeDecorator.new(person)
- puts capitalizedPerson.correct_name
-  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-  puts capitalizedTrimmedPerson.correct_name
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
