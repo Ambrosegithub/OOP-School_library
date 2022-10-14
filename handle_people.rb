77e0f22dd908ef39e20b21111aa393d5539995a5
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-10-14 12:54:40 +0200

    Fix data preservation files

--- handle_people.rb
+++ handle_people.rb
@@ -11,7 +11,7 @@ module HandlePeople
                     type: person.class,
                     age: person.age,
                     name: person.name,
-                    parent_permission: person.parent_permission,
+                    parent_permission: person.parent_permission
                         
                 }
             )
@@ -21,7 +21,7 @@ module HandlePeople
                 type: person.class,
                 age: person.age,
                 name: person.name,
-                specialization: person.specialization,
+                specialization: person.specialization
                 }
             )
         end
